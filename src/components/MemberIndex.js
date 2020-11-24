8acbd19e25740cf0e5e723cceb407d8960bff575
Hannah-86 <hana86.shinee@gmail.com>
2020-11-24 16:36:56 +0900

    bug exists

--- src/components/MemberIndex.js
+++ src/components/MemberIndex.js
@@ -1,14 +1,55 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
+import { readEvents } from '../actions'
 import './member.css'
 
-function MemberIndex() {
-  return(
-    <div className="body">
-      <h1>member</h1>
-    </div>
-  )
+class MemberIndex extends React.Component {
+  constructor(props) {
+    super(props)
+    this.renderEvents = this.renderEvents.bind(this)
+  }
+
+  renderEvents() {
+    const { members } = this.props
+
+    return members.map(member => (
+      <tr key={member.id}>
+        <td>{member.id}</td>
+        <td>
+          <Link to={`members/${member.id}`}>
+            {member.name}
+          </Link>
+        </td>
+        <td>{member.sex}</td>
+        <td>{member.role}</td>
+      </tr>
+    ))
+  }
+
+  render() {
+    return(
+      <div className="body">
+        <h1>member</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>name</th>
+              <th>sex</th>
+              <th>role</th>
+            </tr>
+          </thead>
+        </table>
+        <tbody>
+          {this.renderEvents}
+        </tbody>
+      </div>
+    )
+  }
 }
 
-export default MemberIndex
+const mapStateToProps = state => ({ members : state.members })
+const mapDispatchToProps = ({ readEvents })
+export default connect(mapStateToProps, mapDispatchToProps)(MemberIndex); 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider }from 'react-redux';
 import { createStore } from 'redux';
 
-import reducer form './reducers'
+import reducer from './reducers'
 import './index.css';
 import App from './App';
 
